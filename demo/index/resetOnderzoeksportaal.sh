55a79d494d52f1623f2ac541895b424d1ca73610
Matthijs Brouwer <matthijs@brouwer.info>
2017-03-01 06:48:34 +0100

    remove ignored files

--- demo/index/resetOnderzoeksportaal.sh
+++
@@ -1,6 +0,0 @@
-
-rm -rf /Users/matthijs/www/www.meertens.dev/html/onderzoeksportaaltest/smarty/cache/*
-rm -rf /Users/matthijs/www/www.meertens.dev/html/onderzoeksportaaltest/smarty/templates_c/*
-rm -rf /Users/matthijs/www/www.meertens.dev/html/onderzoeksportaaltest/sites/nederlab/smarty/cache/*
-rm -rf /Users/matthijs/www/www.meertens.dev/html/onderzoeksportaaltest/sites/nederlab/smarty/templates_c/*
-
