7d4ee2305f1096e74a7640917a8f8f0fc0ceb111
Matthijs Brouwer <matthijs@brouwer.info>
2017-11-27 09:34:51 +0100

    facet support for sorted docvalues

--- src/main/java/mtas/codec/util/CodecCollector.java
+++ src/main/java/mtas/codec/util/CodecCollector.java
@@ -70,6 +70,7 @@ import org.apache.lucene.index.LeafReader;
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.index.NumericDocValues;
 import org.apache.lucene.index.PostingsEnum;
+import org.apache.lucene.index.SortedDocValues;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.index.Terms;
 import org.apache.lucene.index.TermsEnum;
@@ -475,34 +476,55 @@ public class CodecCollector {
           //prefer to use pointvalue
           if (!fi.getDocValuesType().equals(DocValuesType.NONE)) {                        
             Iterator<Integer> docIterator = docSet.iterator();
-            //numeric
-            if(fi.getDocValuesType().equals(DocValuesType.NUMERIC)) {
-              Map<Long, List<Integer>> facetDataSubList = new HashMap<>();
-              NumericDocValues docValues = r.getContext().reader()
-                  .getNumericDocValues(entry.getKey());              
-              int docId;
-              while (docIterator.hasNext()) {
-                docId = docIterator.next() - lrc.docBase;
-                if(docValues.advanceExact(docId)) {
-                  long value = docValues.longValue();
-                  if(!facetDataSubList.containsKey(value)) {
-                    List<Integer> facetDataSubListItem = new ArrayList<Integer>();
-                    facetDataSubListItem.add(docId+lrc.docBase);
-                    facetDataSubList.put(value, facetDataSubListItem);                  
-                  } else {
-                    facetDataSubList.get(value).add(docId+lrc.docBase);
-                  }
-                }  
+            //numeric or sorted
+            if(fi.getDocValuesType().equals(DocValuesType.NUMERIC) || fi.getDocValuesType().equals(DocValuesType.SORTED)) {
+              //create map of values to corresponding docIds
+              Map<Object, List<Integer>> facetDataSubList = new HashMap<>();
+              //numeric
+              if(fi.getDocValuesType().equals(DocValuesType.NUMERIC)) {
+                NumericDocValues docValues = r.getContext().reader()
+                    .getNumericDocValues(entry.getKey());              
+                int docId;
+                while (docIterator.hasNext()) {
+                  docId = docIterator.next() - lrc.docBase;
+                  if(docValues.advanceExact(docId)) {
+                    long value = docValues.longValue();
+                    if(!facetDataSubList.containsKey(value)) {
+                      List<Integer> facetDataSubListItem = new ArrayList<Integer>();
+                      facetDataSubListItem.add(docId+lrc.docBase);
+                      facetDataSubList.put(value, facetDataSubListItem);                  
+                    } else {
+                      facetDataSubList.get(value).add(docId+lrc.docBase);
+                    }
+                  }  
+                }
+              //sorted
+              } else if(fi.getDocValuesType().equals(DocValuesType.SORTED)) { 
+                SortedDocValues docValues = r.getContext().reader().getSortedDocValues(entry.getKey());
+                int docId;
+                while (docIterator.hasNext()) {
+                  docId = docIterator.next() - lrc.docBase;
+                  if(docValues.advanceExact(docId)) {
+                    String value = docValues.binaryValue().utf8ToString();
+                    if(!facetDataSubList.containsKey(value)) {
+                      List<Integer> facetDataSubListItem = new ArrayList<Integer>();
+                      facetDataSubListItem.add(docId+lrc.docBase);
+                      facetDataSubList.put(value, facetDataSubListItem);                  
+                    } else {
+                      facetDataSubList.get(value).add(docId+lrc.docBase);
+                    }
+                  }  
+                }
               }
               if(!facetDataSubList.isEmpty()) {
                 SortedMap<String, int[]> facetDataList = entry.getValue();
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
                 sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-                for(Entry<Long,List<Integer>> facetEntry : facetDataSubList.entrySet()) {
+                for(Entry<Object,List<Integer>> facetEntry : facetDataSubList.entrySet()) {
                   int[] docIdList = facetEntry.getValue().stream().mapToInt(i -> i).toArray();
                   String termValue;
                   if(facetDataType.get(entry.getKey()).equals(NumberType.DATE.name())) {
-                    Date date = new Date(facetEntry.getKey());
+                    Date date = new Date((Long) facetEntry.getKey());
                     termValue = sdf.format(date);
                   } else {
                     termValue = facetEntry.getKey().toString();
@@ -521,7 +543,7 @@ public class CodecCollector {
                 }                     
               }
             } else {
-              throw new IOException("faces for docValues of type "+fi.getDocValuesType()+" not implemented");
+              throw new IOException("facets for docValues of type "+fi.getDocValuesType()+" not implemented");
             }              
           } else if(!docSet.isEmpty()) {
             if(facetDataType.get(entry.getKey())
