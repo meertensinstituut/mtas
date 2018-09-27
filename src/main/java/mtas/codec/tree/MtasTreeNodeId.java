cdbde82e64941232463c25c0a1c815ece7c4341f
LB <lhuygi@users.noreply.github.com>
2018-09-27 10:10:54 +0200

    MtasTreeNodeId: unbox ref, modern hash and compareTo

--- src/main/java/mtas/codec/tree/MtasTreeNodeId.java
+++ src/main/java/mtas/codec/tree/MtasTreeNodeId.java
@@ -1,7 +1,12 @@
 package mtas.codec.tree;
 
+import java.util.Objects;
+
+/**
+ * Note: this class has a natural ordering that is inconsistent with equals.
+ */
 public class MtasTreeNodeId implements Comparable<MtasTreeNodeId> {
-  public Long ref;
+  public long ref;
   public int additionalId;
   public long additionalRef;
 
@@ -13,28 +18,25 @@ public class MtasTreeNodeId implements Comparable<MtasTreeNodeId> {
 
   @Override
   public int compareTo(MtasTreeNodeId o) {
-    return ref.compareTo(o.ref);
+    return Long.compare(ref, o.ref);
   }
 
   @Override
   public boolean equals(Object obj) {
-    if (this == obj)
+    if (this == obj) {
       return true;
-    if (obj == null)
+    }
+    if (obj == null || getClass() != obj.getClass()) {
       return false;
-    if (getClass() != obj.getClass())
-      return false;
+    }
     final MtasTreeNodeId that = (MtasTreeNodeId) obj;
-    return ref.equals(that.ref) && additionalId == that.additionalId
-        && additionalRef == that.additionalRef;
+    return ref == that.ref &&
+      additionalId == that.additionalId &&
+      additionalRef == that.additionalRef;
   }
 
   @Override
   public int hashCode() {
-    int h = this.getClass().getSimpleName().hashCode();
-    h = (h * 3) ^ ref.hashCode();
-    h = (h * 5) ^ additionalId;
-    h = (h * 7) ^ (int) additionalRef;
-    return h;
+    return Objects.hash(ref, additionalId, additionalRef);
   }
 }
